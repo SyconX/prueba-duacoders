ad6b81da84efad46ed1902f5ae315aef1674fcdc
javier.fraga <javier.fraga@duacode.com>
2024-01-09 09:21:27 +0100

    Configuración inicial del proyecto, BBDD y variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configura las variables de entorno
+    ConfigModule.forRoot({
+      // Permite inyectar el servicio a nivel global de la app
+      isGlobal: true,
+      // Especifica el archivo para las variables
+      envFilePath: '.env'
+    }),
+    // Configura BBDD
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // Carga las entidades que vamos definiendo
+      synchronize: (process.env.STAGE === 'dev') ? true : false, // Sincronza las tablas según detecta cambios en las entidades
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Configura registro entradas por consola (logs)
+  const logger = new Logger('App');
+
+  // Prefijo global para acceso API
+  app.setGlobalPrefix('api');
+
+  // Configuración pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Solo las propiedades que están explícitamente decoradas con validadores serán consideradas. 
+      whitelist: true,
+      // Si no es propiedad válida (whitelist) lanza excepción BadRequestException
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  // Prepara documentación endpoints api con Swagger
+  const config = new DocumentBuilder()
+  .setTitle('DUACODERS REST API')
+  .setDescription('Api en NodeJS para el proyecto de DUACODERS')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  // Configuración puerto 
+  await app.listen(process.env.PORT);
+  logger.log(`App is running on PORT ${process.env.PORT}`)
 }
 bootstrap();
